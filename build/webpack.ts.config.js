faa7d91633372556be17c92f8c535e95957f8b17
HanChn <670777516@qq.com>
2019-08-15 15:47:24 +0800

    Create webpack.ts.config.js

---
+++ build/webpack.ts.config.js
@@ -0,0 +1,17 @@
+// ts环境配置项
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+};
