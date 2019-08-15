7a6a3ab709b9328ef2748db3858d8977ca2c0806
HanChn <670777516@qq.com>
2019-08-15 15:50:51 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// import "./css/test.css";
+
+// class App {
+//   state = {};
+
+//   constructor() {
+//     console.log("Hello World ! This is App Page !");
+//     let createEl = document.createElement("div");
+//     createEl.innerHTML = "Hello World !";
+//     document.querySelector("body").appendChild(createEl);
+//   }
+
+//   render = () => {
+//     console.log("This is arrow !");
+//   };
+// }
+
+// new App();
+
+// react 示例demo
+
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+
+// class Test extends Component {
+//   render() {
+//     return <h1>Hello World !</h1>;
+//   }
+// }
+
+// render(<Test>Hi</Test>, document.querySelector("#root"));
